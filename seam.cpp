d04b6f97d2b2566209fbbd8dce9a46b61bcec454
Alex K <alexkarrrr@gmail.com>
2020-04-19 12:57:27 -0400

    Add error handling

--- seam.cpp
+++ seam.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include <vector>
+#include <fstream>
 
 int main(int argc, char *argv[])
 {
+    if (argc != 2)
+        std::cout <<"Wrong format! Should be \".\\seam file.pgm\"" <<std::endl;
+    else
+    {
 
+    }
     return 0;
 }
