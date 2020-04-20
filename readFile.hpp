e413665ff3ec66d847dd818361781743d2d6ad30
Alex K <alexkarrrr@gmail.com>
2020-04-20 08:29:12 -0400

    Add class PGM and add function prototypes to header

---
+++ readFile.hpp
@@ -0,0 +1,28 @@
+#ifndef INCLUDE_READFILE_HPP
+#define INCLUDE_READFILE_HPP
+
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <limits>
+
+class PGM
+{
+public:
+    PGM(std::string file) {pgm.open(file);};
+
+    void setCriticalValues();
+    void readPGM();
+    int getX() {return xValue;};
+    int getY() {return yValue;};
+    int getG() {return grayscaleValue;};
+    void printVector();
+private:
+    int xValue = 0, yValue = 0, grayscaleValue = 0;
+    std::string line;
+    std::fstream pgm;
+    std::vector<std::vector<int>> values;
+};
+
+#endif
