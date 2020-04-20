4ef94c3c5660fe9478c6ce6116996f3d2ba9349c
Alex K <alexkarrrr@gmail.com>
2020-04-20 08:30:19 -0400

    Add PGM object and function calls to read in pgm file

--- seam.cpp
+++ seam.cpp
@@ -1,14 +1,24 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <sstream>
+#include <limits>
+#include "readFile.hpp"
 
 int main(int argc, char *argv[])
 {
     if (argc != 2)
-        std::cout <<"Wrong format! Should be \".\\seam file.pgm\"" <<std::endl;
+        std::cout <<"Wrong format! Should be \"./seam file.pgm\"" <<std::endl;
     else
     {
 
+        std::string filename = argv[1];
+        PGM image(filename);
+        image.setCriticalValues();
+        image.readPGM();
+        image.printVector();
+
     }
+
     return 0;
 }
